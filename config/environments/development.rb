f2e702f498312535c936854c3725ae63e9c0ead4
ryoma-desuyo <relicsjcbass4@gmail.com>
2020-01-22 12:10:03 +0900

    modify

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,14 +60,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # mailer setting
-  config.action_mailer.default_url_options = { protocol: 'http', host: '3.115.137.186' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => "rakuichirakusen@gmail.com",
-    :password => "rakuichitanaka000000",
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.default_url_options = { protocol: 'localhost', port: 3000 }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address => "smtp.gmail.com",
+  #   :port => 587,
+  #   :user_name => "rakuichirakusen@gmail.com",
+  #   :password => "rakuichitanaka000000",
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 end
