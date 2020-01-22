8076864db28b84dd7b8b686a100b298f8ac43d00
ryoma-desuyo <relicsjcbass4@gmail.com>
2020-01-22 12:04:55 +0900

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # mailer setting
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { protocol: 'http', host: '3.115.137.186' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
