cc0fb92a9400d8c54d4a1aafd823f853bca36bf1
ryoma-desuyo <relicsjcbass4@gmail.com>
2020-01-24 23:06:34 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :searches, only:[:index]
+  
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'   
