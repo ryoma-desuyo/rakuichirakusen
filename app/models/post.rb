5c82d386217c2613146e3d9e14ddee6eab9d8c52
ryoma-desuyo <relicsjcbass4@gmail.com>
2020-01-28 12:23:49 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :post_tag_relations, dependent: :delete_all
+  has_many :tags, through: :post_tag_relations
+  
   validates :title, presence: true, length: { maximum: 20 }
   validates :body, presence: true, length: { maximum: 1000 }
+
 
   attachment :image
 
